b118c5efe3859998cf3f52d683f0663f6d7542e7
gucheng <gucheng@seatent.com>
2019-11-27 21:20:01 +0800

    fix: 🐛 腾讯真实链接抓取

--- tasks/vip.js
+++ tasks/vip.js
@@ -10,6 +10,14 @@ const task = async () => {
   const page = await browser.newPage();
   // 跳转到腾讯视频vip电影页面
   await page.goto("https://film.qq.com/film_all_list/allfilm.html?type=movie&sort=18");
+
+  await page.setRequestInterception(true);
+  page.on('request', interceptedRequest => {
+    if (interceptedRequest.url().endsWith('.png') || interceptedRequest.url().endsWith('.jpg'))
+      interceptedRequest.abort();
+    else
+      interceptedRequest.continue();
+  })
   // 视频列表选择器
   const listSelector = "ul.figures_list li.list_item .figure_title_score .figure_title a";
   // 等待列表选择器加载完成
@@ -17,19 +25,45 @@ const task = async () => {
     timeout: 5000
   });
 
-  // 通过选择器找到对应列表项的标题和链接
-  const res = await page.$$eval(listSelector, ele =>
-    ele.map(el => ({
-      url: el.href,
-      text: el.innerText,
-    }))
-  );
-  await browser.close();
+
+  // 通过选择器找到对应列表项的标题和链接,点击前往获取真是
+  const arr = await page.$$eval(listSelector, ele => {
+    if (ele.length > 10) {
+      ele = ele.slice(0, 10)
+    }
+    return ele.map((el, i) => {
+      if (i <= 10) {
+        return {
+          url: el.href,
+          text: el.innerText
+        }
+      }
+    })
+  });
+  const videoSelect = '#_pic_title_list_ul a.figure_detail';
+  const promiseArr = arr.map(async item => {
+    const vPage = await browser.newPage();
+    await vPage.goto(item.url)
+    // 等待含有真实链接的部分加载完成
+    await vPage.waitForSelector(videoSelect, {
+      timeout: 5000
+    });
+    const href = await vPage.$eval(videoSelect, ele => ele.href);
+    await vPage.close();
+    return href
+  })
+  const links = await Promise.all(promiseArr);
+  const res = arr.map((item, index) => {
+    return {
+      url: links[index],
+      text: item.text
+    }
+  })
   return {
     typeName: 'Vip影院',
     time: formatDateTime(new Date()),
     data: res
   }
 };
-
+task()
 module.exports = task

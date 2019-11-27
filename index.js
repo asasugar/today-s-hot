ade22919aba737b88f8a46ec2abcc89f9c990778
gucheng <gucheng@seatent.com>
2019-11-27 15:16:06 +0800

    ci: 🎡 使用node-schedule定时任务，shelljs自动提交

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// const schedule = require('node-schedule');
+const schedule = require('node-schedule');
+const shell = require('shelljs');
 const { fileServer } = require("./tools");
 const juejinTask = require("./tasks/juejin");
 const nbaTask = require("./tasks/nba");
@@ -8,8 +9,14 @@ const mainTask = async () => {
   const res = await Promise.all([juejinTask(), nbaTask()])
   fileServer.write(JSON.stringify(res))
 }
-mainTask()
 
-// schedule.scheduleJob('57 * * * *', function () {
-// });
+//每日早上9:30分定时执行一次:
+schedule.scheduleJob('00 30 9 * * *', async () => {
+  await mainTask()
+  shell.exec(`git add .`)
+  shell.exec(`git commit -m "chore: :bento:	更新资讯"`)
+  shell.exec(`git pull`)
+  shell.exec(`git push`)
+  console.log('scheduleCronstyle:' + new Date());
+});
 

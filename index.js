daa3d60d3ad39695a81c1ac5ff4810675efca484
gucheng <gucheng@seatent.com>
2019-12-04 14:43:11 +0800

    fix: 🐛 失败重新执行

--- index.js
+++ index.js
@@ -12,10 +12,20 @@ const mainTask = async () => {
 }
 //每日早上10:30分定时执行一次:
 schedule.scheduleJob('00 30 10 * * *', async () => {
-  await mainTask()
-  shell.exec(`git add .`)
-  shell.exec(`git commit -m "chore: :bento:	更新资讯"`)
-  shell.exec(`git pull`)
-  shell.exec(`git push`)
-  console.log('scheduleCronstyle:' + new Date());
+  try {
+    await mainTask()
+    shell.exec(`git add .`)
+    shell.exec(`git commit -m "chore: :bento:	更新资讯"`)
+    shell.exec(`git pull`)
+    shell.exec(`git push`)
+    console.log('scheduleCronstyle:' + new Date());
+  } catch (error) {
+    await mainTask()
+    shell.exec(`git add .`)
+    shell.exec(`git commit -m "chore: :bento:	更新资讯"`)
+    shell.exec(`git pull`)
+    shell.exec(`git push`)
+    console.log('scheduleCronstyle:' + new Date());
+    throw new Error('执行失败，重新执行...');
+  }
 });
